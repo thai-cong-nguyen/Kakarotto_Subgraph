27bfb5739b472257070528a911ebdab0f06a3a4b
Thai Cong Nguyen <21520169@gm.uit.edu.vn>
2024-08-25 12:01:43 +0700

    Changing ABI and address of Treasure Contract

--- src/data/addresses.ts
+++ src/data/addresses.ts
@@ -1,7 +1,7 @@
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
-export const KakarottoCharacter = "0x297276a7d6f89467574c58ae34e34a8c5e15ac3c"
-export const KakarottoTreasure = "0x89f34087da5805287d9e192ee3d260c5c44a3748"
-export const KakarottoItem = "0x20994a1b0f3aa64fdee575783ba4aa1daa7b14d4"
-export const KakarottoMarketplace = "0x3a5fd464fa05da9388b0c0f5fac24ec5329ffe7e"
-export const ERC721Bid = "0x922ceb9c31cbb84f10826e324222e1d0b3e071d9"
+export const KakarottoCharacter = "0xed4f50ad8898853095203d49b63878d81bfee4d5"
+export const KakarottoTreasure = "0xCF2CFF39E29Da4C0210b16be7e3E717d04ED9A7E"
+export const KakarottoItem = "0x5d3b399b58aa37bae80dbf7303e46c10e80529a7"
+export const KakarottoMarketplace = "0x50b7210384431eebd54d1b94dbd14a4c13763247"
+export const ERC721Bid = "0x3713b9a513d62a10686c5af6cb8e4b7c8c36bd5b"
